f6beab34079d75c9c6c98061a9e9f10e46e93e50
DavidQRy <davidgui393gmail.com>
2025-06-25 13:45:49 -0500

    feat: Configura el entorno de desarrollo inicial y la estructura de proyecto
    - Establece Vite a la versión 6.2.2 para garantizar compatibilidad con @tailwindcss/vite.
    - Instala Tailwind CSS (v3.x), su plugin @tailwindcss/vite (v4.0.14), PostCSS y Autoprefixer.
    - Configura 'tailwind.config.js' con paths para el escaneo de archivos y extiende el tema con fuentes ('Poppins') y colores personalizados ('primary').
    
    - Instala dependencias clave para la aplicación: 'axios', 'moment', 'react-hot-toast', 'react-icons', 'react-router-dom', y 'recharts'.
    - Establece la estructura base de carpetas en 'src/': 'components', 'context', 'hooks', 'pages' (con 'Admin', 'Auth', 'User' subcarpetas), 'routes', y 'utils' (con 'apiPaths', 'axiosInstance', 'data', 'helper', 'uploadImage'

---
+++ frontend/task-Manager/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import SignUp from "./pages/Auth/signUp"
+import Login from "./pages/auth/login"
+import Dashboard from "./pages/Admin/Dashboard"
+import UserDashboard from "./pages/User/Dashboard"
+import ManageTask from "./pages/Admin/ManageTask"
+import ManageUser from "./pages/Admin/ManageUsers"
+import MyTasks from "./pages/User/MyTasks"
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signUp" element={<SignUp />}></Route>
+
+          {/* Admin  Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/task" element={<ManageTask />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/users" element={<ManageUser />} />
+          </Route>
+
+          {/* Users  Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route path="/user/task-details/:id" element={<viewTasksDetails />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/task-Manager/src/pages/Admin/viewTasksDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const viewTasksDetails = () => {
+    return (
+        <div>
+            viewTasksDetails
+        </div>
+    )
+}
+
+export default viewTasksDetails
